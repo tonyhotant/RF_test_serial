59a3a0c9798f46104de5f587a372a2563a3b3fa8
Lisheng Yu <lisheng.yu@nuheara.com>
2019-01-22 13:02:35 +0800

    simified code

--- Stingray_RF_Test.py
+++ Stingray_RF_Test.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 # coding: utf-8
+# Keysight N9000A spectrum analyser OS admin password: Keysight4u!
+# TO DO: add arguments to execute script in 5/10 degree, CW/CCW
 import os
 import time
 import sys
@@ -12,13 +14,19 @@ ser = serial.Serial ('COM1', 9600, timeout = 1) # open configured seiral port
 
 if ser.is_open == True:
     print ("\nserial port is openned\n")
+
+    #ser.write (b'Set Step Size "5.0"\r\n')
+    #ss = ser.read (10)
+    #ss = ss.decode ()
+    #print (ss)
+
     while rotationDegree <= 360 :
         print ("rotation angle is now", rotationDegree)
-        ser.write(b'Step CW\r\n')  # write serial command to turntable
-        time.sleep(11)             # wait at least 11 sec to execute next command
+        ser.write (b'Step CW\r\n')  # write serial command to turntable
+        time.sleep (11)             # wait at least 11 sec to execute next command
         rotationDegree += 10
-        s = ser.read(10)
-        s = s.decode()
+        s = ser.read (10)
+        s = s.decode ()
         print (s)
     if rotationDegree > 360 :
         fullCycleRound += 1
